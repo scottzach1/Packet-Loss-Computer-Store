83ca84f2e2cdac47bdde7a667f4db914a763cf28
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-08 10:58:00 +1300

    Reformat code

--- server/routes/cartServerRoutes.ts
+++ server/routes/cartServerRoutes.ts
@@ -2,17 +2,17 @@ import express, {Request, Response} from 'express';
 
 const router = express.Router();
 
-router.get('/add', [], (req : Request, res: Response) => {
+router.get('/add', [], (req: Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
     return res.send('SOME ACTION');
 });
 
-router.get('/remove', [], (req : Request, res: Response) => {
+router.get('/remove', [], (req: Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
     return res.send('SOME ACTION');
 });
 
-router.get('/clear', [], (req : Request, res: Response) => {
+router.get('/clear', [], (req: Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
     return res.send('SOME ACTION');
 });
