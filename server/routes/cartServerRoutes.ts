7877a648e0c83afa420d97f0dc8b0e8c2584727f
jeffreytong123 <jeffreytong123@hotmail.com>
2020-10-08 14:54:36 +1300

    Attempt at the routes

--- server/routes/cartServerRoutes.ts
+++ server/routes/cartServerRoutes.ts
@@ -9,12 +9,34 @@ router.get('/api/cart/add', [], ((req : Request, res: Response) => {
 
 router.get('/api/cart/remove', [], ((req : Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
-    return res.send('SOME ACTION');
-}));
+    const id = req.params.id
+    User.findById(req.user._id).then((user) => {
+        const {cart} = user
+        const items = cart.items;
+        const mycart = new Cart(cart);
+        let i;
+        for (i in items) {
+            if (items[i].new_item._id == id) {
+                let list = mycart.remove(i);
+                break;
+            }
+        }
+        user.cart = mycart.getObject();
+        user.save();
+        return res.send('Item removed');
+   }});
 
 router.get('/api/cart/clear', [], ((req : Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
-    return res.send('SOME ACTION');
+    User.findById(req.user._id).then((output: { cart: { items: never[]; totalCost: number; }; save: () => void; }) => {
+        output.cart = {items: [], totalCost: 0};
+        output.save();
+        return utils.render(req, res, 'cart', 'Cart', {cart: {items: [], totalCost: 0}});
+    }).catch((error) => {
+        utils.log('error', error);
+        return utils.renderError(req, res, 500, 'Error occured clearing cart');
+    })
+    return res.send('Cart Cleared');
 }));
 
 export {router as cartServerRouter};
