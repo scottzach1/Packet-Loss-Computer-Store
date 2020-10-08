228bacfc9a57c81004238dbe6adfa63d3193a272
jeffreytong123 <jeffreytong123@hotmail.com>
2020-10-08 14:53:13 +1300

    Attempt at the routes

--- server/routes/authServerRoutes.ts
+++ server/routes/authServerRoutes.ts
@@ -4,17 +4,29 @@ const router = express.Router();
 
 router.get('/api/auth/login', [], ((req : Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
-    return res.send('SOME ACTION');
+    if(req.user) {
+        return res.redirect('/');
+    }
+    return utils.render(req, res, 'login', 'Login', {})
 }));
 
 router.get('/api/auth/signup', [], ((req : Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
-    return res.send('SOME ACTION');
+    if(req.user) {
+        return res.redirect('/');
+    }
+    return utils.render(req, res, 'signup', 'Sign Up', {});
 }));
 
 router.get('/api/auth/reset', [], ((req : Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
     return res.send('SOME ACTION');
 }));
+
+router.get('/api/auth/logout/', utils.ensureAuthenticated, (req, res) => {
+    req.session.destroy();
+    return res.redirect('/')
+});
+
 
 export {router as authServerRouter};
--- server/routes/shopServerRoutes.ts
+++ server/routes/shopServerRoutes.ts
@@ -25,7 +25,26 @@ router.post('/api/shop/items/add', [], async (req: Request, res: Response) => {
 
 router.get('/api/shop/items/remove', [], ((req: Request, res: Response) => {
     // TODO: This will need to be implemented in much more depth.
-    return res.send('SOME ACTION');
+    const id = req.params.id
+    User.findById(req.user._id).then((user) => {
+        const {cart} = user
+        const items = cart.items;
+        const mycart = new Cart(cart);
+        let i;
+        for (i in items){
+            if(items[i].new_item._id == id){
+                const list = mycart.remove(i);
+                break;
+            }
+        }
+        user.cart = mycart.getObject();
+        user.save();
+
+        return res.send({delete: true, status: 200})
+    }).catch((error: any) =>{
+        utils.log('error', error);
+        return res.send({delete: false, status: 500, error: 'Failed to delete item'})
+    });
 }));
 
 router.get('/api/shop/items/update', [], ((req: Request, res: Response) => {
