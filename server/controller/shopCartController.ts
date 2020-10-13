21ba0775b4571831bc8b3331a06759c3bfe380dd
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-13 23:07:48 +1300

    Separates express logic from shopCartController for better coherence.
    In this commit, we move the express router logic out of the
    shopCartController. Our justification for this is it made understanding the
    routers more complicated. Furthermore, our security middleware injects the
    user into the req body. Without this middleware then there would be no user
    context. This level of assumed dependence could lead to unforeseen bugs down
    the track.

--- server/controller/shopCartController.ts
+++ server/controller/shopCartController.ts
@@ -1,10 +1,13 @@
 import {ShopCart, ShopCartDoc} from "../models/shopCartModel";
-import {User, UserDoc} from "../models/userModel";
-import {Request, Response} from "express";
+import {UserDoc} from "../models/userModel";
 import {Types} from "mongoose";
 
-
-const createCart = async (user: UserDoc): Promise<ShopCartDoc> => {
+/**
+ * Creates a new cart and associates it with the new `UserDoc`.
+ *
+ * @param user - the user document to create and associate cart with.
+ */
+export const createCart = async (user: UserDoc): Promise<ShopCartDoc> => {
     const cart = new ShopCart({
         userId: user._id,
         items: [],
@@ -17,7 +20,12 @@ const createCart = async (user: UserDoc): Promise<ShopCartDoc> => {
     return cart;
 }
 
-const getCart = async (user: UserDoc): Promise<ShopCartDoc | null> => {
+/**
+ * Gets the `ShopCartDoc` associated with the user. If none exists, one will be created.
+ *
+ * @param user - the user to get / create `ShopCartDoc` for.
+ */
+export const getCart = async (user: UserDoc): Promise<ShopCartDoc | null> => {
     const {cartId} = user;
     if (!cartId) return await createCart(user);
 
@@ -25,14 +33,27 @@ const getCart = async (user: UserDoc): Promise<ShopCartDoc | null> => {
     return (cart) ? cart : await createCart(user);
 }
 
-const clearCart = async (cart: ShopCartDoc): Promise<ShopCartDoc> => {
+/**
+ * Clears the provided `ShopCartDoc`.
+ *
+ * @param cart - the cart to clear.
+ */
+export const clearCart = async (cart: ShopCartDoc): Promise<ShopCartDoc> => {
     cart.items = [];
     await cart.save();
 
     return cart;
 }
 
-const addToCart = async (cart: ShopCartDoc, itemId: string, quantity: number): Promise<ShopCartDoc> => {
+/**
+ * Adds the following `itemId` and `quantity` to the provided cart. If mappings already exist,
+ * all will be wiped and a new entry will be added.
+ *
+ * @param cart - the cart to add the entry to.
+ * @param itemId - the itemId of the `ShopListingDoc` document.
+ * @param quantity - the quantity of the item to save.
+ */
+export const addToCart = async (cart: ShopCartDoc, itemId: string, quantity: number): Promise<ShopCartDoc> => {
     if (!quantity) return await removeFromCart(cart, itemId);
     const objectId = new Types.ObjectId(itemId)
 
@@ -43,127 +64,16 @@ const addToCart = async (cart: ShopCartDoc, itemId: string, quantity: number): P
     return cart;
 }
 
-const removeFromCart = async (cart: ShopCartDoc, itemId: string) => {
+/**
+ * Removes the following `itemId` from the entire `ShopCartDoc`.
+ *
+ * @param cart - the cart to remove the item from.
+ * @param itemId - the itemId of the `ShopListingDoc` to be removed.
+ */
+export const removeFromCart = async (cart: ShopCartDoc, itemId: string) => {
     const objectId = new Types.ObjectId(itemId)
     cart.items = cart.items.filter((item) => !objectId.equals(item.itemId));
     await cart.save();
 
     return cart;
-}
-
-export const getCartHandler = async (req: Request, res: Response) => {
-    const {_id}: any = req.user;
-
-    try {
-        // User authenticated, find within MongoDB.
-        const user = await User.findById(_id);
-        if (!user) throw 'Could not find user!';
-
-        // User found, obtain and clear cart.
-        const cart = await getCart(user);
-        if (!cart) throw 'Failed to find cart for user';
-
-        // Success
-        return res
-            .status(200)
-            .json({cart})
-            .send();
-    } catch (e) {
-        // Failure
-        return res
-            .status(400)
-            .json({errors: [e]})
-            .send();
-    }
-}
-
-export const clearCartHandler = async (req: Request, res: Response) => {
-    const {_id}: any = req.user;
-
-    try {
-        // User authenticated, find within MongoDB.
-        const user = await User.findById(_id);
-        if (!user) throw 'Could not find user!';
-
-        // User found, obtain and clear cart.
-        const cart = await getCart(user);
-        if (!cart) throw 'Failed to create or clear cart for user';
-        await clearCart(cart);
-
-
-        // Success
-        return res
-            .status(200)
-            .json({cart})
-            .send();
-    } catch (e) {
-        // Failure
-        return res
-            .status(400)
-            .json({errors: [e]})
-            .send();
-    }
-}
-
-export const addToCartHandler = async (req: Request, res: Response) => {
-    const {_id}: any = req.user;
-    const {itemId, quantity} = req.body;
-
-    try {
-        // User authenticated, find within MongoDB.
-        const user = await User.findById(_id);
-        if (!user) throw 'Could not find user!';
-
-        // User found, obtain cart.
-        const cart = await getCart(user);
-        if (!cart) throw 'Failed to obtain cart for user';
-
-        // Add item to cart.
-        if (!itemId || (typeof quantity !== 'number'))
-            throw 'Missing `itemId` and `quantity` arguments.'
-        await addToCart(cart, itemId, quantity);
-
-        // Success
-        return res
-            .status(201)
-            .json({cart})
-            .send();
-    } catch (e) {
-        // Failure
-        return res
-            .status(400)
-            .json({errors: [e]})
-            .send();
-    }
-}
-
-export const removeFromCartHandler = async (req: Request, res: Response) => {
-    const {_id}: any = req.user;
-    const {itemId} = req.body;
-
-    try {
-        // User authenticated, find within MongoDB.
-        const user = await User.findById(_id);
-        if (!user) throw 'Could not find user!';
-
-        // User found, obtain cart.
-        const cart = await getCart(user);
-        if (!cart) throw 'Failed to obtain cart for user';
-
-        // Add item to cart.
-        if (!itemId) throw 'Missing `itemId` argument.'
-        await removeFromCart(cart, itemId);
-
-        // Success
-        return res
-            .status(200)
-            .json({cart})
-            .send();
-    } catch (e) {
-        // Failure
-        return res
-            .status(400)
-            .json({errors: [e]})
-            .send();
-    }
 }
