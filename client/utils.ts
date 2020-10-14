821578cc31704142be6182eeeb127d039d36443b
jeffreytong123 <jeffreytong123@hotmail.com>
2020-10-15 03:34:26 +1300

    password reset temp

--- client/utils.ts
+++ client/utils.ts
@@ -1,16 +1,73 @@
-import {Request, Response} from 'express';
+const tempUser = require('tempUser');
+
+const User = require('./user');
+
+const logger = tempUser.createLogger({ // create the logger, log to files and the console
+    level: 'info',
+    format: tempUser.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new tempUser.transports.File({filename: 'logs/error.log', level: 'error'}),
+        new tempUser.transports.File({filename: 'logs/combined.log'}),
+        new tempUser.transports.Console({format: tempUser.format.simple()})
+    ]
+});
 
 /**
- * A stylised render that applies the general layout, as well as a mechanism to pass information to the page.
- *
- * @param req - The request from Nodejs
- * @param res - The response from Nodejs
- * @param page -The path of the page within the `client/views` page.
- * @param title - The title of the web page.
- * @param data - Any additional data passed to the EJS webpage.
+ * Render a page
+ * @param {Request} req
+ * @param {Response} res
+ * @param {string} page
+ * @param {string} title
+ * @param {Object} data
  */
-const render = (req: Request, res: Response, page: string, title: string, data: any) => {
-    res.render('layout', {page, title, data, user: null});
+const render = (req: { user: { _id: any } }, res: { status?: any; render?: (arg0: string, arg1: { page?: any; title: any; data: any; user: any }) => any }, page: string, title: string, data: { error: any; code: any }) => {
+    if(req.user) {
+        User.findById(req.user._id).then((output) => { // get the user
+            return res.render('layout', {page, title, data, user: output});
+        }).catch((error) => {
+            return res.render('error', {title: `Error ${500}`, data: {error: 'Internal Server Error', code: 500}, user: output});
+        })
+    } else {
+        return res.render('layout', {page, title, data, user: undefined});
+    }
 };
 
-export {render};
+/**
+ * Render an error page
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Number} code
+ * @param {string} error
+ */
+const renderError = (req: { user: { _id: any; }; }, res: { status?: any; render?: (arg0: string, arg1: { page?: any; title: any; data: any; user: any; }) => any; }, code: any, error: any) => {
+    res.status(code)
+    return render(req, res, 'error', `Error ${code}`, {error, code});
+}
+
+/**
+ * Log a message to the logger
+ * @param {string} level the level to log
+ * @param {any} message message to log
+ */
+const log = (level: any, message: any) => logger.log(level, message);
+
+module.exports = {
+    render,
+    renderError,
+    log,
+
+    /**
+     * Ensure that the user is authenticated
+     * @param {Request} req
+     * @param {Response} res
+     * @param {void} next
+     */
+    ensureAuthenticated: (req: { isAuthenticated: () => any; }, res: { redirect: (arg0: string) => any; }, next: () => void) => {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            return res.redirect('/api/auth/login');
+        }
+    },
+};
--- server/routes/authServerRoutes.ts
+++ server/routes/authServerRoutes.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from 'express';
 import {loginHandler, signupHandler} from "../controller/authController";
+import {render} from "ejs";
 
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
 router.post('/login', [], async (req: Request, res: Response) => {
@@ -21,9 +23,119 @@ router.post(`/signup`, [], async (req: Request, res: Response) => {
     return res.status(code).json(response).send();
 });
 
-router.post('/reset', [], (req: Request, res: Response) => {
-    // TODO: This will need to be implemented in much more depth.
-    return res.send('TODO: Needs to be implemented!');
+// router.post('/reset', [], (req: Request, res: Response) => {
+//     // TODO: This will need to be implemented in much more depth.
+//     return res.send('TODO: Needs to be implemented!');
+// });
+
+router.get('/forgot', [], async (req: Request, res: Response) => {
+    if(req.query.token) {
+        User.findOne({token: req.query.token}).then((output) => {
+            if(!output || output.authType !== 'local') return res.redirect('/login'); // user not found or not password auth
+            if (output.token === req.query.token) { // can reset passord
+                return render(req, res, 'reset', 'Reset Password', {token: req.query.token});
+            }
+            return renderError(req, res, 400, 'Invalid reset token');
+        }).catch((error) => {
+            log('error', error);
+            return renderError(req, res, 500, 'Something went wrong');
+        });
+    } else {
+        return render(req, res, 'forgot', 'Forgot Password', {});
+    }
 });
+
+/**
+ * Log a message to the logger
+ * @param {string} level the level to log
+ * @param {any} message message to log
+ */
+const log = (level: any, message: any) => logger.log(level, message);
+
+module.exports = {
+    render,
+    renderError,
+    log,
+
+    /**
+     * Ensure that the user is authenticated
+     * @param {Request} req
+     * @param {Response} res
+     * @param {void} next
+     */
+    ensureAuthenticated: (req: { isAuthenticated: () => any; }, res: { redirect: (arg0: string) => any; }, next: () => void) => {
+        if (req.isAuthenticated()) {
+            next();
+        } else {
+            return res.redirect('/login');
+        }
+    },
+};
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    }
+});
+
+router.post('/sendReset', (req, res) => {
+    const current_date = (new Date()).valueOf().toString();
+    const random = Math.random().toString();
+    const token = crypto.createHash('sha1').update(current_date + random).digest('hex');
+    User.findOne({email: req.body.email}).then((output: { authType: string; token: any; save: () => void; }) => {
+        if(!output || output.authType !== 'local') return res.redirect('/forgot'); // silent fail exit (user doesnt exist or facebook auth)
+        output.token = token;
+        output.save(); // save the user
+
+        const email = { // build the email
+            from: process.env.EMAIL_EMAIL,
+            to: req.body.email,
+            subject: 'Computer Store Password Reset',
+            text: `Hello, you have requested a password reset. Navigate to \`/forgot?token=${token}\` to reset your password. You can also paste the token \`${token}\` into the token field of /forgot`,
+        };
+
+        // send the email
+        transporter.sendMail(email).then((output: { accepted: any; }) => {
+            log('info', 'Sent email');
+            log('info', output);
+            if(!output.accepted) {
+                log('error', 'Email send failed');
+            }
+            return res.redirect('/forgot'); // valid email
+        }).catch((error: any) => {
+            log('error', error);
+            return renderError(req, res, 500, 'Email send failed');
+        });
+    }).catch((error: any) => {
+        log(error);
+        return renderError(req, res, 500, 'Something went wrong');
+    });
+});
+
+/**
+ * POST /reset/
+ * Checks the token supplied and resets the password if valid
+ */
+router.post('/reset', (req, res) => {
+    if(req.body.newPass1 !== req.body.newPass2) {
+        return renderError(req, res, 400, 'Passwords do not match');
+    }
+    bcrypt.hash(req.body.newPass1, 10, (err: any, hash: any) => { // generate new password
+        User.findOne({token: req.body.token}).then((output: { token: undefined; password: any; save: () => void; }) => { // find the user with that token
+            if(!output) return res.redirect('/login'); // silent fail exit
+            output.token = undefined; // clear token
+            output.password = hash; // update the password
+            output.save(); // save the user
+            return res.redirect('/login');
+        }).catch((error: any) => {
+            log(error);
+            return renderError(req, res, 500, 'Something went wrong');
+        });
+    });
+});
+
 
 export {router as authServerRouter};
