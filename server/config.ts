a5cea58fd7527a1d988634713ef4fd5724e9c8b1
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-18 09:15:14 +1300

    Adds prod database and improves env / launch scripts
    In this commit, we add a new production database to isolate
    development from the live website. We also now add some new
    entries into the package.json start scripts. On top of this,
    the developer is now warned when .env values cannot be found.

--- server/config.ts
+++ server/config.ts
@@ -2,14 +2,64 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+/**
+ * Select correct MongoDB URI.
+ */
+let mongoUrl: string | undefined;
+
+switch (process.env.NODE_ENV) {
+    case "production":
+        mongoUrl = process.env.MONGO_PROD_URI;
+        break;
+    case "development":
+        mongoUrl = process.env.MONGO_DEV_URI;
+        break;
+    case "testing":
+        mongoUrl = process.env.MONGO_TEST_URI;
+        break;
+    default:
+        mongoUrl = process.env.MONGO_DEV_URI;
+}
+
+if (!mongoUrl) {
+    console.warn('No Mongo URI could be found, defaulting to `localhost`.');
+    mongoUrl = `mongodb://localhost/computer-parts-store`;
+}
+
+/**
+ * Select correct server port.
+ */
+let serverPort = process.env.SERVER_PORT;
+if (!serverPort) {
+    serverPort = "3000";
+    console.warn(`No server port was specified, defaulting to ${serverPort}`)
+}
+
+/**
+ * Warn for other missing variables.
+ */
+if (!process.env.JWT_SECRET)
+    console.warn('No JWT secret could be found, defaulting to unsafe string!')
+
+if (!process.env.GOOGLE_OAUTH2_CLIENT_ID)
+    console.warn('No Google OAuth2 Client ID could be found.')
+
+if (!process.env.GOOGLE_OAUTH2_CLIENT_SECRET)
+    console.warn('No Google OAuth2 Client Secret could be found.')
+
+if (!process.env.GOOGLE_PLACES_API_KEY)
+    console.warn('No Google Places API key could be found.')
+
+if (!process.env.IPINFO_API_KEY)
+    console.warn('No IpInfo API key could be found.')
+
 export default {
     JWT_SECRET: process.env.JWT_SECRET || 'jwtSampleSecretToken15%^$8',
     MONGO_DB: {
-        URI: ((process.env.MONGO_TEST) ? process.env.MONGO_TEST_URI : process.env.MONGO_DEV_URI)
-            || `mongodb://localhost/computer-parts-store`,
+        URI: mongoUrl,
     },
     SERVER: {
-        PORT: 3000
+        PORT: serverPort,
     },
     GOOGLE_OAUTH2: {
         CLIENT_ID: process.env.GOOGLE_OAUTH2_CLIENT_ID || 'INVALID',
