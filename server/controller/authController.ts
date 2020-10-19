1a0367acfca6756ea6a87751328ab878c9c5b5ce
Zac Scott <scottzach1@myvuw.ac.nz>
2020-10-19 13:18:34 +1300

    Adds password reset link expiry
    In this commit, we add password reset expiry by adding
    another field in the userdoc for reset expire.

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -5,6 +5,7 @@ import config from "../config";
 import passwordStrength from 'check-password-strength';
 import bcrypt from "bcrypt";
 import EmailHandler from '../emails';
+import {addDays} from "date-fns";
 
 /**
  * Interface defining the object structure of a response from this file.
@@ -204,6 +205,7 @@ export const generateResetLinkHandler = async (user: UserDoc) => {
 
   // Update user document with seed.
   user.resetSeed = hash;
+  user.resetExpire = addDays(new Date(), 1);
   await user.save();
 
   // Send email to client!
@@ -232,23 +234,35 @@ Computer Store
  * @param password - the desired password.
  */
 export const redeemResetLinkHandler = async (seed: string, password: string) => {
-  let user = await User.findOne({resetSeed: seed});
+  try {
+    let user = await User.findOne({resetSeed: seed});
+    const expDate = user?.resetExpire;
 
-  if (!user) return {errors: ['invalid reset link!'], success: false};
+    if (!user || !expDate) throw 'invalid reset link!';
 
-  const resp = checkPasswordComplexity(password);
+    if (new Date().getTime() > expDate.getTime())
+      throw 'reset link has expired!'
 
-  if (!resp.success)
-    return resp;
+    const resp = checkPasswordComplexity(password);
 
-  user.resetSeed = '';
-  user.password = password;
+    if (!resp.success)
+      return resp;
 
-  user = await user.save();
+    user.resetSeed = '';
+    user.resetExpire = undefined;
+    user.password = password;
 
-  resp.success = true;
+    user = await user.save();
 
-  return user;
+    resp.success = true;
+
+    return user;
+  } catch (e) {
+    return {
+      errors: [e],
+      success: false,
+    }
+  }
 }
 
 
